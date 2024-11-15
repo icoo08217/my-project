ff3544c4c7578b60b4f994b66ed601cefdbac013
byeongchan.jeon <icoo08217@naver.com>
2024-11-15 21:19:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/my_project/bc/BcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_project.bc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
