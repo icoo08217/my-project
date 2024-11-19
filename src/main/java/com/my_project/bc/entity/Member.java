3bb86f6f747988ff3b7fd942421664bc6c0b71d4
byeongchan.jeon <icoo08217@naver.com>
2024-11-19 23:12:01 +0900

    예시 Home 컨트롤러 생성 , Member 클래스 생성

--- src/main/java/com/my_project/bc/entity/Member.java
+++ src/main/java/com/my_project/bc/entity/Member.java
@@ -4,11 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter
+@RequiredArgsConstructor
 @Entity
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String address;
 }
